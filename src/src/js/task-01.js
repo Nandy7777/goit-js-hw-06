5b3a1ccf73eddac783ee95e3ce05106767861e61
Nandy7777 <nandy7777@ukr.net>
2022-08-06 12:23:41 +0300

    -src

--- src/src/js/task-01.js
+++
@@ -1,9 +0,0 @@
-const list = document.querySelectorAll('li.item');
-console.log(`Number of categories: ${list.length}`);
-const ul = document.querySelectorAll('#categories>li');
-ul.forEach(el => {
-  const category = `Category: ${el.firstElementChild.textContent}`;
-  const elements = `Elements: ${el.lastElementChild.children.length}`;
-  console.log(category);
-  console.log(elements);
-});
