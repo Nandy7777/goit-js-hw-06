e11f4cba0b89856f8679314570324730850ecf03
Nandy7777 <nandy7777@ukr.net>
2022-08-06 15:16:22 +0300

    6

---
+++ js/task-06.js
@@ -0,0 +1,15 @@
+const inputEl = document.querySelector('#validation-input');
+const inputLength = inputEl.dataset.length;
+const input = document.querySelector('input');
+
+inputEl.addEventListener('blur', e => {
+  const text = e.target.value;
+
+  if (text.length === +inputLength) {
+    inputEl.classList.add('valid');
+    inputEl.classList.remove('invalid');
+  } else {
+    inputEl.classList.remove('valid');
+    inputEl.classList.add('invalid');
+  }
+});
